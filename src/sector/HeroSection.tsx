ed12b237d35aaf58b1af2b71097994a134dc3047
abir9756 <abirhasan9756@gmail.com>
2026-09-14 05:23:59 +0600

    seventh

--- src/sector/HeroSection.tsx
+++ src/sector/HeroSection.tsx
@@ -7,17 +7,17 @@ const HeroSection = () => {
 
     return (<>
         
-    <div className='container mx-auto md:mx-0 my-24 text-center  md:text-start flex flex-col justify-between  items-center md:flex-row'>
-        <div className="text-center md:text-start flex  flex-col items-start ">
+    <div className='container mx-auto my-24 text-center  md:text-start flex flex-col justify-between  items-center md:flex-row'>
+        <div className="text-center  md:text-start flex justify-center md:justify-start items-center flex-col md:items-start ">
                 <p className='font-extrabold text-6xl'> Build Your Ideal</p>
                 <p className='pb-6  font-extrabold text-6xl bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent'>Development Stack</p>
                 <p className='pb-10 text-[#475569] '>Explore frontend, backend, database, and tooling options,<br></br>
                 compare them side by side, and put together the stack that fits your<br></br>
                 next project.</p>
-            {/* <div className='pt-3 flex gap-4  '> */}
+            <div className=' md:flex  flex flex-row justify-center mx-auto md:mx-0 items-center '>
                 <button className="btn rounded-md py-4 px-3 mr-3 mt-3 text-white  bg-linear-to-r from-[#F97316] to-[#EC4899] ">Explore Technologies</button>
                 <button className="btn rounded-md py-4 px-3 mt-3 text-[#475569]">Learn More</button>
-            {/* </div> */}
+            </div>
         </div>
             <div>
                 <img src={Banner} />
